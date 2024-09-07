aac4e287e14135c2ef5d76bb6cd6d259f1b166ca
Isaac Lewis <mail@isaaclewis.me>
2024-09-07 17:37:21 -0500

    spawn and draw enemies

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const base = 1;
 const difficulty = 2;
 const scale = 50;
+const tick = 600;
 
 function isBitActive(number, bitPosition) {
         const mask = 1 << bitPosition;
@@ -23,6 +24,12 @@ function doubleValuesIfKeyExists(obj1, obj2) {
 }>}
  */
 const tileDict = new Map();
+
+tileDict.set(-1, {
+        name: "spawn",
+        backgroundColor: "indigo",
+        enemyTravel: false,
+});
 
 tileDict.set(0, {
         name: "none",
@@ -70,7 +77,7 @@ towerDict.set(2, {
         name: "mage",
         damage: base * 4,
         speed: base * 4,
-        backgroundColor: "purple",
+        backgroundColor: "yellow",
 });
 
 /**
@@ -94,7 +101,7 @@ enemyDict.set(2, {
         name: "troll",
         health: base * 8 * (difficulty / 5 + 1),
         speed: base * 2 * (difficulty / 10 + 1),
-        backgroundColor: "lightbrown",
+        backgroundColor: "brown",
 });
 
 enemyDict.set(4, {
@@ -109,6 +116,10 @@ function PX(value) {
 
 class Program {
         /**
+         * @type {[number, number][]}
+         */
+        spawnPoints = [];
+        /**
          * @type {undefined | [number, number]}
          */
         selectedTile;
@@ -121,8 +132,8 @@ class Program {
         map = [
                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-                [1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                [-1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                [-1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1],
                 [0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
                 [0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
                 [0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0],
@@ -136,16 +147,40 @@ class Program {
                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         ];
+        mapE = [
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ];
 
         /**
          * @type {HTMLElement}
          */
         board;
+        /**
+         * @type {HTMLElement}
+         */
+        boardE;
         // posX = 100;
         // posY = 100;
         constructor() {
                 this.board = document.getElementById("board");
+                this.boardE = document.getElementById("boardE");
                 this.DrawTiles();
+                this.spawnPoints = this.DetermineSpawnPoints();
         }
 
         DrawTiles() {
@@ -156,6 +191,30 @@ class Program {
                                 this.DrawTile(x, y);
                         }
                 }
+        }
+
+        DrawTile(x, y) {
+                // console.log("drawing tile", x, y);
+                let tileKey = this.map[x][y];
+                let tile = tileDict.get(tileKey);
+
+                let el = document.createElement("div");
+                el.classList.add("tile");
+                if (tile.name === "none") {
+                        el.classList.add("tile-modifiable");
+                        el.addEventListener("contextmenu", (event) => {
+                                event.preventDefault();
+                                this.DrawAddTowerContextMenu(x, y);
+                                this.selectedTile = [x, y];
+                        });
+                }
+                el.style.height = PX(base);
+                el.style.width = PX(base);
+                el.style.backgroundColor = tile.backgroundColor;
+                el.style.top = PX(x);
+                el.style.left = PX(y);
+
+                this.board.appendChild(el);
         }
 
         RemoveAllDrawAddTowerContextMenu() {
@@ -183,44 +242,12 @@ class Program {
                 this.board.appendChild(el);
                 return el;
         }
-
-        DrawTile(x, y) {
-                // console.log("drawing tile", x, y);
-                let tileKey = this.map[x][y];
-                let tile = tileDict.get(tileKey);
-
-                let el = document.createElement("div");
-                el.classList.add("tile");
-                if (tile.name === "none") {
-                        el.classList.add("tile-modifiable");
-                        el.addEventListener("contextmenu", (event) => {
-                                event.preventDefault();
-                                this.DrawAddTowerContextMenu(x, y);
-                                this.selectedTile = [x, y];
-                        });
-                }
-                el.style.height = PX(base);
-                el.style.width = PX(base);
-                el.style.backgroundColor = tile.backgroundColor;
-                el.style.top = PX(x);
-                el.style.left = PX(y);
-
-                this.board.appendChild(el);
-        }
-
-        ProgressRound() {
-                console.log("progressing round");
-                this.round += 1;
-                let enemys = this.DetermineEnemy(this.round);
-                console.log(" * * * enemys", enemys);
-        }
-
-        DetermineEnemy(r) {
+        DetermineEnemiesForRound(r) {
                 let enemyCounts = {};
                 for (let i = 1; i <= r + 1; i++) {
                         const enemy = i - 1;
                         if (i < r) {
-                                let o = this.DetermineEnemy(enemy);
+                                let o = this.DetermineEnemiesForRound(enemy);
                                 enemyCounts = doubleValuesIfKeyExists(
                                         enemyCounts,
                                         o
@@ -237,18 +264,114 @@ class Program {
                 return enemyCounts;
         }
 
+        /**
+         * @returns {[number, number]}
+         */
+        DetermineSpawnPoints() {
+                let options = [];
+                for (let x = 0; x < this.map.length; x++) {
+                        const row = this.map[x];
+                        for (let y = 0; y < row.length; y++) {
+                                let tileKey = this.map[x][y];
+                                let tile = tileDict.get(tileKey);
+                                if (tile.name === "spawn") {
+                                        options.push([x, y]);
+                                }
+                        }
+                }
+                return options;
+        }
+
+        /**
+         *
+         * @returns {[number, number]}
+         */
+        PickRandomSpawn() {
+                return this.spawnPoints[
+                        Math.floor(Math.random() * this.spawnPoints.length)
+                ];
+        }
+
+        DrawEnemies() {
+                for (let x = 0; x < this.mapE.length; x++) {
+                        const row = this.mapE[x];
+                        for (let y = 0; y < row.length; y++) {
+                                const col = row[y];
+                                this.DrawEnemy(x, y);
+                        }
+                }
+        }
+
+        DrawEnemy(x, y) {
+                let unit = this.mapE[x][y];
+                if (!unit) {
+                        return;
+                }
+
+                let el = document.createElement("div");
+                el.classList.add("tile-unit");
+                // if (tile.name === "none") {
+                //         el.classList.add("tile-unit");
+                //         el.addEventListener("contextmenu", (event) => {
+                //                 event.preventDefault();
+                //                 this.DrawAddTowerContextMenu(x, y);
+                //                 this.selectedTile = [x, y];
+                //         });
+                // }
+                el.style.height = PX(base);
+                el.style.width = PX(base);
+                el.style.backgroundColor = unit.backgroundColor;
+                el.innerText = unit.name;
+                el.style.top = PX(x);
+                el.style.left = PX(y);
+
+                this.boardE.appendChild(el);
+        }
+
+        AddEnemyToMap(unit) {
+                let spawnPoint = this.PickRandomSpawn();
+                this.mapE[spawnPoint[0]][spawnPoint[1]] = unit;
+        }
+
+        /**
+         * @param {{[x:number]:number}} enemies
+         */
+        AddEnemiesToMap(enemies) {
+                let units = Object.entries(enemies);
+                // console.log(units);
+                for (let i = 0; i < units.length; i++) {
+                        let enemy = units[i];
+                        let unitIdentifier = parseInt(enemy[0]);
+                        let unitCount = enemy[1];
+                        let unit = enemyDict.get(unitIdentifier);
+                        for (let j = 0; j < unitCount; j++) {
+                                this.AddEnemyToMap(unit);
+                        }
+                }
+        }
+
+        ProgressRound() {
+                console.log("progressing round");
+                this.round += 1;
+                let enemies = this.DetermineEnemiesForRound(this.round);
+                this.AddEnemiesToMap(enemies);
+                this.DrawEnemies();
+        }
+
+        MainLoop() {
+                let spawning = true;
+                while (spawning) {
+                        setTimeout(() => {
+                                spawning;
+                        }, 1000);
+                }
+        }
+
         Run() {
                 this.ProgressRound();
                 this.ProgressRound();
-                this.ProgressRound();
-                this.ProgressRound();
-                this.ProgressRound();
-                this.ProgressRound();
-                this.ProgressRound();
-                this.ProgressRound();
-                this.ProgressRound();
-                this.ProgressRound();
-                this.ProgressRound();
+
+                // setInterval(() => {}, tick);
         }
 }
 
